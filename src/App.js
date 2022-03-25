e74bce2479190484d9e478c4551bc5640292dc8f
ZaidKhan144 <zaidirfan.945@gmail.com>
2022-03-25 17:06:44 -0500

    section 3 ps desktop

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.scss';
 
 import Section1PS from './layout/PremiumSeating/Section1PS';
 import Section2PS from './layout/PremiumSeating/Section2PS';
+import Section3PS from './layout/PremiumSeating/Section3PS';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <main role="main">
         <Section1PS />
         <Section2PS />
+        <Section3PS />
       </main>
     </div>
   );
---
+++ src/layout/PremiumSeating/Section3PS/index.js
@@ -0,0 +1,3 @@
+import Section3PS from "./Section3PS";
+
+export default Section3PS
