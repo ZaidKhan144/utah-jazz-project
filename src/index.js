e56e502da92d1f59df3db0c19b9325bbe47a963b
ZaidKhan144 <zaidirfan.945@gmail.com>
2022-03-28 13:25:00 -0500

    basename

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/utah-jazz-project'>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
